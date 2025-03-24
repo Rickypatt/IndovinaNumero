8292a8824a995c28325e4249d2566a82f0a264cb
riccardopattono <s307811@studenti.polito.it>
2025-03-24 10:14:01 +0100

    Lezione del 24/03

--- controller.py
+++ controller.py
@@ -1,7 +1,68 @@
 from view import View
 from model import Model
 import flet as ft
+
 class Controller(object):
     def __init__(self, view: View):
         self._view = view
         self._model = Model()
+
+    def reset(self,e):
+        self._model.reset()
+        self._view._txtOutT.value = self._model.T
+        self._view._lv.controls.clear()
+        self._view._btnPlay.disabled = False
+        self._view._txtIn.disabled = False
+        self._view._lv.controls.append(ft.Text("Indovina a quale nuemro sto pensando!"))
+
+        self._view.update()
+
+    def play(self,e):
+        tentativoStr = self._view._txtIn.value
+        self._view._txtIn.value = ""
+        self._view._txtOutT.value = self._model.T - 1
+
+        if tentativoStr == "":
+            self._view._lv.controls.append(ft.Text("Attenzione! Inerisci un valore numerico da testare.", color="red"))
+            self._view.update()
+            return
+
+        try:
+            tentativoInt = int(tentativoStr)
+        except ValueError:
+            self._view._lv.controls.append(ft.Text("Attenzione! Valore inserito non è un intero.", color = "red"))
+            return
+
+        res = self._model.play(tentativoInt)
+
+        if res == 0:
+            self._view._lv.controls.append(ft.Text(f"Fantastico! Hai vinto, il segreto era {tentativoInt}", color="green"))
+            self._view._btnPlay.disabled = True
+            self._view._txtIn.disabled = True
+            self._view.update()
+
+        elif res == 2:
+            self._view._lv.controls.append(ft.Text(f"Mi dispiace, hai finito le vite. Il segreto era {self._model.segreto}"))
+            self._view._btnPlay.disabled = True
+            self._view._txtIn.disabled = True
+            self._view.update()
+
+        elif res == -1:
+            self._view._lv.controls.append(ft.Text(f"Il segreto è più piccolo di {tentativoInt}"))
+            self._view.update()
+
+        else:
+            self._view._lv.controls.append(ft.Text(f"Il segreto è più grande di {tentativoInt}"))
+            self._view.update()
+
+
+
+
+    def getNMax(self):
+        return self._model.NMax
+
+    def getTMax(self):
+        return self._model.TMax
+
+
+
--- model.py
+++ model.py
@@ -2,4 +2,56 @@ import random
 
 class Model(object):
     def __init__(self):
-        pass
+        self._NMax = 100
+        self._TMax = 6
+        self._T = self._TMax
+        self._segreto = None
+
+    def reset(self):
+        #Questo metodo resetta il gioco in qualsiasi momento
+        self._segreto = random.randint(0, self._NMax)
+        self._T = self._TMax
+        print(self._segreto)
+
+    def play(self, guess):
+        """
+        Funzione che segue un step degl gioco
+        :param guess: int
+        :return: 0 se vinto, 1 se segreto è più grande, -1 se segreto è più piccolo, 2 se ho finito le vite
+        """
+        #Da fuori ci arriva un tentativo, lo confrontiamo con il segreto
+
+        self._T -= 1
+
+        if guess == self._segreto:
+            return 0 #Ho vinto
+
+        if self._T == 0:
+            return 2 #Ho perso definitivamente
+
+        if guess > self._segreto:
+            return -1 #Il segreto è più piccolo
+
+        return 1 #Il segreto è più grande
+
+    @property
+    def NMax(self):
+        return self._NMax
+
+    @property
+    def TMax(self):
+        return self._TMax
+
+    @property
+    def T(self):
+        return self._T
+
+    @property
+    def segreto(self):
+        return self._segreto
+
+
+if __name__ == "__main__":
+    m = Model()
+    m.reset()
+    print(m.play(50))
